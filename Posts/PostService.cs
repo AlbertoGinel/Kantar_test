4e13b6718ecf712c7ce3b60719180c5c7d9ea8b1
Alberto Ginel Salvador <aginelsalvador@gmail.com>
2023-05-16 13:30:13 +0300

    PostService sends posts to IndexPost

--- Posts/PostService.cs
+++ Posts/PostService.cs
@@ -1,6 +1,41 @@
-﻿namespace Kantar_test.Posts
+﻿using Kantar_test.Posts;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using static System.Net.WebRequestMethods;
+
+namespace Kantar_test.Posts
 {
-    public class PostService
+    public class PostService : IPostService
     {
+        private readonly HttpClient _httpClient;
+
+        public PostService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<List<PostItem>> GetPosts()
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts");
+                response.EnsureSuccessStatusCode();
+                var data = await response.Content.ReadAsStringAsync();
+                var posts = JsonSerializer.Deserialize<List<PostItem>>(data);
+                if (posts is null || posts.Count == 0)
+                {
+                    throw new InvalidOperationException("No posts found.");
+                }
+
+                return posts;
+            }
+            catch (Exception ex)
+            {
+                // Handle the default case
+                throw;
+            }
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Kantar_test;
+using Kantar_test.Posts;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IPostService, PostService>();
 
 await builder.Build().RunAsync();
