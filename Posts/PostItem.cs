fbc3f49702c63bf5f7e1971dc32dde2bccbcabfe
Alberto Ginel Salvador <aginelsalvador@gmail.com>
2023-05-16 12:55:29 +0300

    PostItem done

--- Posts/PostItem.cs
+++ Posts/PostItem.cs
@@ -1,6 +1,12 @@
-﻿namespace Kantar_test.Posts
+﻿using Microsoft.AspNetCore.Components;
+
+namespace PostsApp.Posts
 {
     public class PostItem
     {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string? title { get; set; }
+        public string? body { get; set; }
     }
 }
